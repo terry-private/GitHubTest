9b4af2993621be58be97fd7a539a9979a5581392
teruhito <ataeteruhito@gmail.com>
2021-01-10 11:31:51 +0900

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by 若江照仁 on 2021/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
